7390dbc365a83dec6783ecc27cbd76d787beda62
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-21 18:07:11 -0800

    Add message view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Message
+from .serializers import MessageSerializer
 
 # Create your views here.
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all().order_by('-date')
+    serializer_class = MessageSerializer
