5674b8c849715442ab3434ab3778f85bba75b57c
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-21 18:34:32 -0800

    Fix import

--- messengerBackend/urls.py
+++ messengerBackend/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from views import MessageViewSet
+from chat.views import MessageViewSet
 
 router = DefaultRouter()
 router.register(r'messages', MessageViewSet)
