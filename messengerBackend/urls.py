6a941dcb67bfe759288c80831b4b116b03dac49c
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-21 18:38:50 -0800

    Fix routers

--- messengerBackend/urls.py
+++ messengerBackend/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from chat.views import MessageViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'messages', MessageViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('router.urls')),
+    path('api/', include(router.urls)),
 ]
